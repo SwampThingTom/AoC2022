d8bc4f2fcda18e2d52ea3397e2ef7b8f78343553
Thomas H Aylesworth <thomas.aylesworth@gmail.com>
2022-12-07 22:32:49 -0500

    Add directory template

---
+++ template/README.md
@@ -0,0 +1,24 @@
+# Advent of Code 2022
+## [Day <day>](https://adventofcode.com/2022/day/<day>) - TBD
+
+### Programming Language 
+
+[<language>](https://en.wikipedia.org/wiki/<language>) TBD
+
+### Why I Chose It
+
+TBD
+
+### How It Went
+
+TBD
+
+### Tools
+
+[<tool>](<tool-link>)
+
+### Running
+
+```
+TBD
+```
---
+++ template/add-day.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Sets up the directory for a new day of Advent of Code 2022
+#
+# Usage:
+# add-day <day-num> <language>
+
+day=$1
+language=$2
+
+target_dir="$day-TBD"
+
+day_num=`expr $day + 0`rm
+mkdir $target_dir
+cp template/README.MD $target_dir
+sed -i '' "s/<day>/$day_num/g" $target_dir/README.md
+sed -i '' "s/<language>/$language/g" $target_dir/README.md
+touch $target_dir/input.txt
