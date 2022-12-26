1f1fe53283b1498ee486b88e77eaef9c3e51086e
Thomas H Aylesworth <thomas.aylesworth@gmail.com>
2022-12-26 17:34:03 -0500

    2022 Day 11 Part 2

--- 11-Monkey/Monkey.tcl
+++ 11-Monkey/Monkey.tcl
@@ -39,106 +39,120 @@ proc throwTo {target item} {
     return $t
 }
 
-# Parse input into monkey dictionaries.
-set i 0
-set monkeyArgs [list]
-set monkeyIndex 0
-foreach line [readInput] {
-    set tokens [split $line]
-    switch $i {
-        1 {
-            set itemsStr [regsub -all { } [join [lrange $tokens 4 end]] ""]
-            set items [split $itemsStr ","]
-            lappend monkeyArgs $items
-          }
-        2 {
-            set operation [lrange $tokens 5 end]
-            lappend monkeyArgs [string map {old "$old"} $operation]
-          }
-        3 {
-            set test [lindex $tokens end]
-            lappend monkeyArgs $test
-          }
-        4 {
-            set ifTrue [lindex $tokens end]
-            lappend monkeyArgs $ifTrue
-          }
-        5 {
-            set ifFalse [lindex $tokens end]
-            lappend monkeyArgs $ifFalse
-          }
-    }
-    set i [expr $i + 1]
-    if {$i == 6} {
-        set monkey [makeMonkey {*}$monkeyArgs]
-        dict set monkeys $monkeyIndex $monkey
-        set monkeyIndex [expr $monkeyIndex + 1] 
-    }
-    if {$i == 7} {
-        set i 0
-        set monkeyArgs [list]
-    }
-}
-
-for {set i 0} {$i < 20} {incr i} { 
-    foreach index [dict keys $monkeys] {
-        set monkey [dict get $monkeys $index]
-        debugPrint "Monkey #$index"
-
-        # Take turn for monkey.
-        set items [dict get $monkey items]
-        set inspected [dict get $monkey inspected]
-        foreach old $items {
-            debugPrint "  Inspect item with worry level of $old."
-            set inspected [expr $inspected + 1]
-           
-            set new [expr [dict get $monkey operation]]
-            debugPrint "    New worry level is $new."
-
-            set new [expr $new / 3]
-            debugPrint "    Monkey bored, worry level is now $new."
-            
-            if { [expr $new % [dict get $monkey test]] == 0 } {
-                set targetIndex [dict get $monkey ifTrue]
-                debugPrint "    Throwing to monkey $targetIndex"
-
-                set target [dict get $monkeys $targetIndex]
-                dict set monkeys $targetIndex [throwTo target $new]
-            } else {
-                set targetIndex [dict get $monkey ifFalse]
-                debugPrint "    Throwing to monkey $targetIndex"
-                
-                set target [dict get $monkeys $targetIndex]
-                dict set monkeys $targetIndex [throwTo target $new]
+proc solve {count reduceWorry} {
+    # Parse input into monkey dictionaries.
+    set i 0
+    set monkeyArgs [list]
+    set monkeyIndex 0
+    foreach line [readInput] {
+        set tokens [split $line]
+        switch $i {
+            1 {
+                set itemsStr [regsub -all { } [join [lrange $tokens 4 end]] ""]
+                set items [split $itemsStr ","]
+                lappend monkeyArgs $items
+            }
+            2 {
+                set operation [lrange $tokens 5 end]
+                lappend monkeyArgs [string map {old "$old"} $operation]
+            }
+            3 {
+                set test [lindex $tokens end]
+                lappend monkeyArgs $test
+            }
+            4 {
+                set ifTrue [lindex $tokens end]
+                lappend monkeyArgs $ifTrue
+            }
+            5 {
+                set ifFalse [lindex $tokens end]
+                lappend monkeyArgs $ifFalse
             }
         }
+        set i [expr $i + 1]
+        if {$i == 6} {
+            set monkey [makeMonkey {*}$monkeyArgs]
+            dict set monkeys $monkeyIndex $monkey
+            set monkeyIndex [expr $monkeyIndex + 1] 
+        }
+        if {$i == 7} {
+            set i 0
+            set monkeyArgs [list]
+        }
+    }
 
-        dict set monkey inspected $inspected
-        dict set monkey items {}
-        dict set monkeys $index $monkey
+    # Calculate gcf of test values.
+    # (Only used for part 2.)
+    set gcf 1
+    dict for {index monkey} $monkeys {
+        set gcf [expr $gcf * [dict get $monkey test]]
+    }
 
+    # Run for count rounds.
+    for {set i 0} {$i < $count} {incr i} {
+        foreach index [dict keys $monkeys] {
+            set monkey [dict get $monkeys $index]
+            debugPrint "Monkey #$index"
+
+            # Take turn for monkey.
+            set items [dict get $monkey items]
+            set inspected [dict get $monkey inspected]
+            foreach old $items {
+                debugPrint "  Inspect item with worry level of $old."
+                set inspected [expr $inspected + 1]
+            
+                set new [expr [dict get $monkey operation]]
+                debugPrint "    New worry level is $new."
+
+                set new [expr $reduceWorry]
+                debugPrint "    Monkey bored, worry level is now $new."
+                
+                if { [expr $new % [dict get $monkey test]] == 0 } {
+                    set targetIndex [dict get $monkey ifTrue]
+                    debugPrint "    Throwing to monkey $targetIndex"
+
+                    set target [dict get $monkeys $targetIndex]
+                    dict set monkeys $targetIndex [throwTo target $new]
+                } else {
+                    set targetIndex [dict get $monkey ifFalse]
+                    debugPrint "    Throwing to monkey $targetIndex"
+                    
+                    set target [dict get $monkeys $targetIndex]
+                    dict set monkeys $targetIndex [throwTo target $new]
+                }
+            }
+
+            dict set monkey inspected $inspected
+            dict set monkey items {}
+            dict set monkeys $index $monkey
+            debugPrint ""
+        }
+
+        dict for {index monkey} $monkeys {
+            set items [dict get $monkey items]
+            set inspected [dict get $monkey inspected]
+            debugPrint "Monkey #$index ($inspected): $items"
+        }
         debugPrint ""
     }
 
+    set max1 0
+    set max2 0
     dict for {index monkey} $monkeys {
-        set items [dict get $monkey items]
         set inspected [dict get $monkey inspected]
-        debugPrint "Monkey #$index ($inspected): $items"
+        if { $inspected > $max1 } {
+            set max2 $max1
+            set max1 $inspected
+        } elseif { $inspected > $max2 } {
+            set max2 $inspected
+        }
     }
-    debugPrint ""
+
+    return [expr $max1 * $max2]
 }
 
-set max1 0
-set max2 0
-dict for {index monkey} $monkeys {
-    set inspected [dict get $monkey inspected]
-    if { $inspected > $max1 } {
-        set max2 $max1
-        set max1 $inspected
-    } elseif { $inspected > $max2 } {
-        set max2 $inspected
-    }
-}
-
-set part1 [expr $max1 * $max2]
+set part1 [solve 20 "\$new / 3"]
 puts "Part 1: $part1"
+
+set part2 [solve 10000 "\$new % \$gcf"]
+puts "Part 2: $part2"
